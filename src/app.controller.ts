32c3fff4f9ea1b0bef08a4dd6faeafa807de2f67
sontx13 <sontx13@gmail.com>
2023-08-17 08:19:01 +0700

    init project

---
+++ src/app.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get,Render } from '@nestjs/common';
+import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService,
+    private configService: ConfigService
+    ) {}
+
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
+  @Get()
+  @Render("home")
+  getHello() {
+    //return 'this.appService.getHello()';
+  }
+  
+}
---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  //MongooseModule.forRoot('mongodb+srv://sontx13:Sonphuong1710@cluster0.bp5irmg.mongodb.net/?retryWrites=true&w=majority')
+  imports: [MongooseModule.forRootAsync({
+              imports: [ConfigModule],
+              useFactory:async (configService:ConfigService) => ({
+                uri: configService.get<string>('MONGO_URL'),
+              }),
+              inject:[ConfigService]
+            }),
+            ConfigModule.forRoot({
+              isGlobal:true
+            })  
+            ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
