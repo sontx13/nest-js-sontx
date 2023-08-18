a15e710efd00d974acea8a5fdc81c45e5a092ba6
sontx13 <sontx13@gmail.com>
2023-08-18 22:06:58 +0700

    create company

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';  
 import { softDeletePlugin } from 'soft-delete-plugin-mongoose';
+import { CompaniesModule } from './companies/companies.module';
 
 @Module({
   //MongooseModule.forRoot('mongodb+srv://sontx13:Sonphuong1710@cluster0.bp5irmg.mongodb.net/?retryWrites=true&w=majority')
@@ -24,7 +25,8 @@ import { softDeletePlugin } from 'soft-delete-plugin-mongoose';
               isGlobal:true
             }),
             UsersModule,
-            AuthModule  
+            AuthModule,
+            CompaniesModule  
             ],
   controllers: [AppController],
   providers: [AppService],
