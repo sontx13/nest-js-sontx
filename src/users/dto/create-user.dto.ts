b69d490515fec1bdd6866f0b8e0e769b131e7052
sontx13 <sontx13@gmail.com>
2023-08-21 10:02:27 +0700

    update user schema

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,20 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsEmail, IsNotEmpty, IsNotEmptyObject, IsObject, ValidateNested } from 'class-validator';
+import mongoose from 'mongoose';
 // class == obj
-export class CreateUserDto {
+
+class Company{
+    @IsNotEmpty({message: 'Id không được để trống'})
+    _id: mongoose.Schema.Types.ObjectId;
+
+    @IsNotEmpty({message: 'Email không được để trống'})
+    email: string;
+}
+
+export class    CreateUserDto {
+    @IsNotEmpty({message: 'Name không được để trống'})
+    name: string;
+
     @IsEmail({},{message: 'Email không đúng định dạng'})
     @IsNotEmpty({message: 'Email không được để trống'})
     email: string;
@@ -8,6 +22,42 @@ export class CreateUserDto {
     @IsNotEmpty({message: 'Password không được để trống'})
     password: string;
 
-    name: string;
+    @IsNotEmpty({message: 'Age không được để trống'})
     age: number;
+
+    @IsNotEmpty({message: 'Gender không được để trống'})
+    gender: number;
+
+    @IsNotEmpty({message: 'Address không được để trống'})
+    address: number;
+
+    @IsNotEmpty({message: 'Role không được để trống'})
+    role: string;
+  
+    @IsNotEmptyObject()
+    @IsObject()
+    @ValidateNested()
+    @Type(()=>Company)
+    company: Company;
+}
+
+export class    RegisterUserDto {
+    @IsNotEmpty({message: 'Name không được để trống'})
+    name: string;
+
+    @IsEmail({},{message: 'Email không đúng định dạng'})
+    @IsNotEmpty({message: 'Email không được để trống'})
+    email: string;
+
+    @IsNotEmpty({message: 'Password không được để trống'})
+    password: string;
+
+    @IsNotEmpty({message: 'Age không được để trống'})
+    age: number;
+
+    @IsNotEmpty({message: 'Gender không được để trống'})
+    gender: number;
+
+    @IsNotEmpty({message: 'Address không được để trống'})
+    address: number;
 }
