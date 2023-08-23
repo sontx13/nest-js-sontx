90ebea9a8befca98c001b05180df690660645496
sontx13 <sontx13@gmail.com>
2023-08-23 14:35:45 +0700

    resumes base

--- src/resumes/dto/create-resume.dto.ts
+++ src/resumes/dto/create-resume.dto.ts
@@ -1,1 +1,39 @@
-export class CreateResumeDto {}
+import { Type } from 'class-transformer';
+import { IsEmail, IsMongoId, IsNotEmpty, IsNotEmptyObject, IsObject, ValidateNested } from 'class-validator';
+import mongoose from 'mongoose';
+// class == obj
+
+export class    CreateResumeDto {
+    @IsEmail({},{message: 'Email không đúng định dạng'})
+    @IsNotEmpty({message: 'Email không được để trống'})
+    email: string;
+
+    @IsNotEmpty({message: 'userId không được để trống'})
+    userId: mongoose.Schema.Types.ObjectId;
+
+    @IsNotEmpty({message: 'Url không được để trống'})
+    url: string;
+
+    @IsNotEmpty({message: 'Status không được để trống'})
+    status: string;
+
+    @IsNotEmpty({message: 'CompanyId không được để trống'})
+    companyId: mongoose.Schema.Types.ObjectId;
+
+    @IsNotEmpty({message: 'JobId không được để trống'})
+    jobId: mongoose.Schema.Types.ObjectId;
+  
+}
+
+export class    CreateUserCVDto {
+    @IsNotEmpty({message: 'Url không được để trống'})
+    url: string;
+
+    @IsNotEmpty({message: 'CompanyId không được để trống'})
+    @IsMongoId({message: 'CompanyId is a mongo id'})
+    companyId: mongoose.Schema.Types.ObjectId;
+
+    @IsNotEmpty({message: 'JobId không được để trống'})
+    @IsMongoId({message: 'CompanyId is a mongo id'})
+    jobId: mongoose.Schema.Types.ObjectId;
+}
--- src/resumes/resumes.module.ts
+++ src/resumes/resumes.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ResumesService } from './resumes.service';
 import { ResumesController } from './resumes.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Resume, ResumeSchema } from './schemas/resume.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Resume.name, schema: ResumeSchema }])],
   controllers: [ResumesController],
   providers: [ResumesService]
 })
